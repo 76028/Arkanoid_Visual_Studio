a09d3d0d9d30c4843d0c3b652fb09310f8b3563c
76028 <47855925+76028@users.noreply.github.com>
2021-02-02 11:30:58 +0100

    Arkanoid

---
+++ Arkanoid/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Arkanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pilka = new System.Windows.Forms.Panel();
+            this.paletka = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pilka
+            // 
+            this.pilka.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pilka.BackgroundImage")));
+            this.pilka.Location = new System.Drawing.Point(369, 566);
+            this.pilka.Name = "pilka";
+            this.pilka.Size = new System.Drawing.Size(30, 30);
+            this.pilka.TabIndex = 0;
+            // 
+            // paletka
+            // 
+            this.paletka.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("paletka.BackgroundImage")));
+            this.paletka.Location = new System.Drawing.Point(333, 594);
+            this.paletka.Name = "paletka";
+            this.paletka.Size = new System.Drawing.Size(110, 5);
+            this.paletka.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(330, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wynik:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(432, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 37);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(693, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Życia:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(753, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(281, 322);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 55);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(231, 375);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 37);
+            this.label6.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(794, 611);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.paletka);
+            this.Controls.Add(this.pilka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pilka;
+        private System.Windows.Forms.Panel paletka;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Arkanoid/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        private Panel[,] bloczki;
+
+        private int kierunekX = 2;
+        private int kierunekY = 2;
+        private int paletkaX = 3;
+        private int rows = 8;
+        private int cols = 8;
+        private int wynik = 0;
+        private int zycia = 3;
+        private Random r;
+        private Color[] colors;
+
+        public Form1()
+        {
+            colors = new Color[4] { Color.BurlyWood, Color.CadetBlue, Color.CornflowerBlue, Color.Goldenrod };
+            r = new Random();
+            generujBloczki();
+            InitializeComponent();
+        }
+
+        private void generujBloczki()
+        {
+            int blockHeight = 30;
+            int blockWidth = 100;
+
+            bloczki = new Panel[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    bloczki[i, j] = new Panel();
+
+                    bloczki[i, j].Width = blockWidth;
+                    bloczki[i, j].Height = blockHeight;
+                    bloczki[i, j].Top = blockHeight * i + 75;
+                    bloczki[i, j].Left = blockWidth * j;
+                    bloczki[i, j].BackColor = colors[r.Next(0, colors.Length)];
+                    bloczki[i, j].BorderStyle = BorderStyle.Fixed3D;
+
+                    this.Controls.Add(bloczki[i, j]);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pilka.Left += kierunekX;
+            pilka.Top += kierunekY;
+            label2.Text =  wynik.ToString();
+            label4.Text = zycia.ToString();
+
+            if (pilka.Top + pilka.Height >= ClientSize.Height)
+            {
+                if (zycia > 0)
+                {
+                    zycia--;
+                    pilka.Left = 369;
+                    pilka.Top = 566;
+                    paletka.Left = 333;
+                    label5.Text = "skucha !";
+                    label6.Text = "zostało jeszcze " + zycia + " żyć";
+                }
+                else
+                {
+                    label5.Text = "Game Over !";
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                }
+            }
+
+            if (pilka.Left <= 0 || pilka.Left + pilka.Width >= ClientSize.Width)
+                kierunekX = -kierunekX;
+            if (pilka.Top <= 0)
+                kierunekY = -kierunekY;
+
+            //uderza w srodek
+            if (pilka.Top + pilka.Height >= paletka.Top &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 > paletka.Left + paletka.Width - 60 &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 <= paletka.Left + paletka.Width - 50 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height)
+            {
+                kierunekX = 0;
+                kierunekY = -3;
+            }
+
+            //jesli pilka leci prosto
+
+            if (pilka.Top + pilka.Height >= paletka.Top &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2) + 5 >= paletka.Left &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2) - 5 <= paletka.Left + paletka.Width - 100 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX <= 0 && kierunekX == 0)
+            {
+                kierunekX = -3;
+                kierunekY = -2;
+            }
+
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 > paletka.Left + paletka.Width - 100 &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 <= paletka.Left + paletka.Width - 60 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX == 0)
+            {
+                kierunekX = -3;
+                kierunekY = -2;
+            }
+
+
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 > paletka.Left + paletka.Width - 50 &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 <= paletka.Left + paletka.Width - 10 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX == 0)
+            {
+                kierunekX = 3;
+                kierunekY = -2;
+            }
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2) + 10 > paletka.Left + paletka.Width - 10 &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2) - 10 <= paletka.Left + paletka.Width &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX == 0)
+            {
+                kierunekX = 3;
+                kierunekY = -2;
+            }
+
+
+
+
+            //pilka leci od lewej do prawej
+
+
+            if (pilka.Top + pilka.Height >= paletka.Top &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)+50 >= paletka.Left &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)-5 <= paletka.Left + paletka.Width - 100 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX<=0 && kierunekX>0)
+            {
+                kierunekX = 3;
+                kierunekY = -2;
+            }
+
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 > paletka.Left + paletka.Width - 100 &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 <= paletka.Left + paletka.Width - 60 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX > 0)
+            {
+                kierunekX = 3;
+                kierunekY = -2;
+            }
+
+
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 > paletka.Left + paletka.Width - 50 &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 <= paletka.Left + paletka.Width - 10 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX > 0)
+            {
+                kierunekX = 3;
+                kierunekY = -2;
+            }
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)+5 > paletka.Left + paletka.Width - 10 &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)-5 <= paletka.Left + paletka.Width &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX > 0)
+            {
+                kierunekX = 4;
+                kierunekY = -1;
+            }
+
+
+            
+
+
+            // pilka leci od prawej do lewej
+
+            if (pilka.Top + pilka.Height >= paletka.Top &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)+5 >= paletka.Left &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)-5 <= paletka.Left + paletka.Width - 85 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX <= 0 && kierunekX < 0)
+            {
+                kierunekX = -4;
+                kierunekY = -1;  
+            }
+
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 > paletka.Left + paletka.Width - 85 &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 <= paletka.Left + paletka.Width - 60 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX < 0)
+            {
+                kierunekX = -3;
+                kierunekY = -2;
+                
+            }
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 > paletka.Left + paletka.Width - 50 &&
+                (pilka.Left + pilka.Width + pilka.Left) / 2 <= paletka.Left + paletka.Width - 25 &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX < 0)
+            {
+                kierunekX = -3;
+                kierunekY = -2;
+            }
+            else if (pilka.Top + pilka.Height >= paletka.Top &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)+5 > paletka.Left + paletka.Width - 25 &&
+                ((pilka.Left + pilka.Width + pilka.Left) / 2)-5 <= paletka.Left + paletka.Width &&
+                pilka.Top + pilka.Height <= paletka.Top + paletka.Height && kierunekX < 0)
+            {
+                kierunekX = -3;
+                kierunekY = -2;
+            }
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (bloczki[i, j].Visible == true)
+                        if (pilka.Left <= bloczki[i, j].Left + bloczki[i, j].Width && pilka.Left+pilka.Width >= bloczki[i, j].Left &&
+                            pilka.Top + 14 <= bloczki[i, j].Top + bloczki[i, j].Height && pilka.Top + pilka.Height - 14 >= bloczki[i, j].Top)
+                        {
+                            Console.WriteLine("bokiem");
+                            bloczki[i, j].Visible = false;
+                            kierunekX = -kierunekX;
+                            wynik++;
+                            label5.Text = "";
+                            label6.Text = "";
+                        }
+
+                        else if ((pilka.Left + pilka.Width + pilka.Left) / 2 >= bloczki[i, j].Left &&
+                            (pilka.Left + pilka.Width + pilka.Left) / 2 <= bloczki[i, j].Left + bloczki[i, j].Width && pilka.Top <= bloczki[i, j].Top + bloczki[i, j].Height &&
+                            pilka.Top + pilka.Height >= bloczki[i, j].Top)
+                        {
+                            bloczki[i, j].Visible = false;
+                            kierunekY = -kierunekY;
+                            wynik++;
+                            label5.Text = "";
+                            label6.Text = "";
+                        }
+                       
+                        
+                }
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            paletka.Left += paletkaX;
+            if (paletka.Left <= 0 || paletka.Left + paletka.Width >= ClientSize.Width)
+                paletkaX = -paletkaX;
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                paletkaX = -paletkaX;
+        }
+
+
+
+    }
+}
